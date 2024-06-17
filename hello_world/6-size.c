e1380d07990ff7f9c4ab0c929414c46c9e475f17
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-17 14:06:54 +0200

    Size

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+/*
+ * main - principal fonction
+ *
+ * Return: fonction void so 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(int));
+	printf("Size of a char: %d byte(s)\n", sizeof(long int));
+	printf("Size of a char: %d byte(s)\n", sizeof(float));
+
+	return (0);
+}
