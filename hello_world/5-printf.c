e923d2f3bdefb1a0a0e58ef762460c48066e2282
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-17 13:38:09 +0200

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - principal fonction
+ *
+ * Return: return 0 because void
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
