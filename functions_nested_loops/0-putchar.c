7e2c12a5cb814d256b69af9dfd5e78c20871a44d
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-18 16:15:16 +0200

    Write a program that prints _putchar, followed by a new line

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main -
+ * main - Write a program that prints _putchar, followed by a new line
  *
- * Return: 
+ * Return: Always 0 because void
  */
 
 int main(void)
 {
-	_putchar();
+	int _putchar(int c);
 	return (0);
 }
