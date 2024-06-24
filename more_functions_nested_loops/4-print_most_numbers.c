39cb61a7e4ee8468c2cbdc2b77dfe352f1f53023
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-24 11:32:03 +0200

    Write a function that prints the numbers, from 0 to 9 without 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ * 
+ * Return: without return beacause void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	_putchar('\n');
+	}
+}
