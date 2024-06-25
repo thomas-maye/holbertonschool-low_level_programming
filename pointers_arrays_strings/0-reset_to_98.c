edeb87bd3560972d8c05abfe466266114269ab74
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-25 10:50:30 +0200

    A function to updates a value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int
+ * @n: is an integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+## Week 4
+# C - Pointers, arrays and strings
