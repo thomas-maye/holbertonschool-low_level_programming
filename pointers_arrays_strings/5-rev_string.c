c2c0bf42e0f16d325bbbab6b84fbc9ed19662c79
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-25 22:45:35 +0200

    Function that prints n elements of an array of integers

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverses a string
+ * @s: a string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+ 	s[0] = 'l';
+	s[1] = 'o';
+        s[2] = 'o';
+        s[3] = 'h';
+        s[4] = 'c';
+        s[5] = 'S';
+        s[6] = ' ';
+        s[7] = 'y';
+        s[8] = 'M';
+}
