f6ed2825945bbc84d9340908261fd5eaf0e5db5f
Thomas Maye <thomas.maye.33@gmail.com>
2024-06-26 15:38:42 +0200

    Function that copies the string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - Function that copies the string pointed
+ * @dest: pointer
+ * @src: pointer
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
