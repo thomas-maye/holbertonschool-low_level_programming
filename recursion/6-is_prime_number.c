e0e7cb5a1836a07a81e966ad2d03c0d633adc3e7
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-03 16:09:34 +0200

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - Write a function that returns 1 if the
+ * input integer is a prime number, otherwise return 0
+ * verify_prime_number - verify if a is a prime number
+ * @n: the input integer
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (verify_prime_number(n, 2));
+}
+
+/**
+ * verify_prime_number - verify if a is a prime number
+ * @n: the input integer
+ * @a: test if a is a prime number
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+
+int verify_prime_number(int n, int a)
+{
+	if (a == n)
+	{
+		return (1);
+	}
+	else if (n % a == 0)
+	{
+		return (0);
+	}
+	else if (a * a == n)
+        {
+                return (n);
+        }
+	return (verify_prime_number(n, a + 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -59,5 +59,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_find(int n, int i);
+int is_prime_number(int n);
+int verify_prime_number(int n, int a);
 
 #endif
