661b54149de40d00d9a1b2d597e9d3265a6fd42b
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-08 13:57:18 +0200

    Copy of a string given a parameter

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * *_strdup : function that returns a pointer to a newly allocated
+ * *_strdup - function that returns a pointer to a newly allocated
  * space in memory, which contains a copy of the string given as
  * a parameter.
  * @str: a string
