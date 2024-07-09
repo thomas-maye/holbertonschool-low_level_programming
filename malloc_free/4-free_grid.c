40f407f7110c7e75d9e5b3c4ffa2cb5ee6b73a87
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-09 10:37:50 +0200

    Function that frees a 2 dimensional grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -13,12 +13,12 @@ void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	if (grid == NULL || height <= 0)
+	if (grid != NULL || height > 0)
 	{
 		for (i = 0; i < height ; i++)
 		{
-		free(grid[i]);
+			free(grid[i]);
 		}
-		free(grid);
 	}
+	free(grid);
 }
