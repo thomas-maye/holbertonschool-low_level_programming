55b55eae6a5a6c8aa5014d8d2acf6ae629ecc465
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-15 13:24:48 +0200

    Function executes a function

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -71,5 +71,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
