ada9745f1de824f649c991edc27dcccbec573dc8
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-15 16:28:49 +0200

    The programm that perform simple operations

---
+++ function_pointers/get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include "calc.h"
+
+/**
+ * get_op_func - the function that selects the correct function
+ * @s : the operator
+ * Return: the pointer to the function that correspond to the operator
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (i < 5)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (*ops[i].f);
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ function_pointers/op_functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ * op_add - Function that returns the sum
+ * @a: a number
+ * @b: a other number
+ * Return: the result of the operation
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that returns the difference
+ * @a: a number
+ * @b: a other number
+ * Return: the result of the operation
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function that returns the product
+ * @a: a number
+ * @b: a other number
+ * Return: the result of the operation
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that returns the result of the division
+ * @a: a number
+ * @b: a other number
+ * Return: the result of the operation
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that remainder of the division
+ * @a: a number
+ * @b: a other number
+ * Return: the result of the operation
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
