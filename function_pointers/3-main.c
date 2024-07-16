c39e3ff47cdeceb226bc2f0ebd9aa5826bbaf4d7
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-16 13:13:32 +0200

    The calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,9 +22,8 @@ int main(int argc, char *argv[])
 	}
 
 	a = atoi(argv[1]);
-        b = atoi(argv[3]);
-
-        operation = get_op_func(argv[2]);
+	b = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
 
 	if (!operation)
 	{
