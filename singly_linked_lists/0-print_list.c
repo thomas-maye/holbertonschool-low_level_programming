ef58f6db2206fc33fb4294cdfa92f7d67be0a51d
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-29 10:32:24 +0200

    The first commit of the project Singly Linked Lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - A function that prints all the elements
+ * of a list_t
+ * 
+ *
+ *
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+
+}
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#ifndef linkedlists_h
+#define linkedlists_h
+
+#include <stdio.h>
+
+size_t print_list(const list_t *h);
+
+#endif
+
