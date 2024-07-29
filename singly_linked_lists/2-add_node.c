b1dc381e1d60c5f6c46025979a745afde279faa0
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-29 14:47:51 +0200

    The first commit of the task to add a new node at the end

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - A function that adds a new node
+ * at the begginning of a list_t list
+ * @head: the double pointer to the head of the list
+ * @str: the string
+ * Return: the address of the element or NULL if
+ * it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len;
+
+	/*calcul la longueur de la str*/
+	for (len = 0; str[len] != '\0'; len++)
+
+	/*allocation de mÃmoire d'un noeud*/
+	/*affecte aussi l'adresse de la mÃmoire allouÃe */
+	new_node = malloc(sizeof(list_t));
+
+	/*vÃrification si la mÃ©moire est biern allouÃe*/
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		/*initialisation du new_node*/
+		new_node->str = strdup(str);
+		new_node->len = len;
+		new_node->next = (*head);
+		/*met a jour le pointer du head list vers le new_node*/
+		(*head) = new_node;
+	}
+	return (*head);
+}
