09628ac1dcbd1d09e77db149201582795470f78d
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-31 15:32:54 +0200

    A function that delete a node at the defined place

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,5 +13,43 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *temp;
+	unsigned int i = 0;
 
+	temp = *head;
+
+	/* Verif si la liste est vide*/
+	if (*head == NULL)
+		return (-1);
+
+	/*pour supprimer le head*/
+	if (index == 0)
+	{
+		/*deplace le head*/
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+
+	/*Chercher le noeud de l'index donne*/
+	while (temp != NULL && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	/*Si temp est null, on a trop de noeud*/
+	if (temp == NULL)
+		return (-1);
+
+	/*Supprime le noeud de l'index*/
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+
+	free(temp);
+	return (1);
 }
