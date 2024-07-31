7efa58b87cf9c6d3cd74ed0b3d254b4fbbeb1eaa
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-31 11:21:46 +0200

    My first commit of the function that returns the sum of all the date

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of all
+ * the data (n) of a linked list
+ * @head: the pointer of the head of the list
+ * Return: if the list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
+
---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - A function that inserts
+ * a new node at a given position
+ * @h: the double pointer to the head of the list
+ * @idx: the index of the list 
+ * @n: a number
+ * Return: the address of the new node, or NULL if it failed
+ *
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	
+}
